1462ce7989ebbc2341fb95646a4728945bf4cd87
Harshad Kshirsagar <harshadkshirsagar173@gmail.com>
2025-09-26 14:20:34 +0530

    reset form when invalid credentials

--- College_records_app/views.py
+++ College_records_app/views.py
@@ -11,7 +11,7 @@ def ajax_login_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return JsonResponse({'message': 'Session expired. Please log in again.', 'status': 440})
+            return JsonResponse({'message': 'Session expired. Please log in again.', 'status': 302})
         return view_func(request, *args, **kwargs)
     return _wrapped_view
 
@@ -241,7 +241,6 @@ def user_login(request):
             }
             return JsonResponse(response_data)
         
-
         
 def user_logout(request):
     if request.method == 'POST':
