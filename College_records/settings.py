a40a14db5b0a89459b1e2f71aa0e4570d897db71
Harshad Kshirsagar <harshadkshirsagar173@gmail.com>
2026-01-01 17:15:18 +0530

    security risk addressed

--- College_records/settings.py
+++ College_records/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,11 +141,16 @@ CSRF_COOKIE_SAMESITE = "Lax"
 CSRF_COOKIE_SECURE = False  # True if HTTPS
 
 
+load_dotenv()
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "malharmr123@gmail.com"
-EMAIL_HOST_PASSWORD = "ijbeexzgnoybytro"
-DEFAULT_FROM_EMAIL = "malharmr123@gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+print("EMAIL USER:", os.getenv("EMAIL_HOST_USER"))
+
 
